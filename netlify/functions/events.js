dc42a74d38a1b0e013d15a267962395a46d2e370
Eric Sakmar <esakmar@truefit.io>
2022-03-31 16:09:08 -0400

    removes referrer check for now

--- netlify/functions/events.js
+++ netlify/functions/events.js
@@ -11,28 +11,11 @@ const getEvents = async source => {
 };
 
 exports.handler = async function(event, _context) {
-  console.log(event.headers.referrer);
-
-  if (
-    !(
-      process.env.NETLIFY_DEV === "true" ||
-      (event.headers.referrer &&
-        event.headers.referrer.includes("pgh-live.netlify.app"))
-    )
-  ) {
-    return {
-      statusCode: 401,
-      body: JSON.stringify("Unauthorized")
-    };
-  }
-
   const results = await Promise.all([
     getEvents(spirit),
     getEvents(smalls),
     getEvents(thunderbird)
   ]);
-
-  // console.log(results[2]);
 
   const events = results.flatMap(r => r);
 
